c27454aeb410b9d4ff9a52d1993727564e34b552
unclepete-20 <pedroparty310@gmail.com>
2022-04-19 12:15:37 -0600

    Se ha configurado la plantilla base del proyecto con React

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  //Template to create the Amazon UI using React.js 
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  mode: "development",
+  // Entre point of project 
+  entry: "./src/index.jsx",
+  // Name and path of output file
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "bundle_react_amazon"),
+  },
+  // Loaders for different file types
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css"
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+    })
+  ],
+  module: {
+    rules: [
+      // Loader for css files
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      // Loader for js files
+      {
+        test: /\.(js|jsx)$/i,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      // Loader for images and files
+      {
+        test: /\.(png|jpe?g|gif|mp4)$/i,
+        loader: 'file-loader',
+        options: {
+          outputPath: 'assets',
+        },
+      },
+    ],
+  },
+};
