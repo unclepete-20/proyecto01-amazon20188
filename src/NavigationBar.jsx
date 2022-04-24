4d932919ea889402665375da37f1ffff587a64ad
unclepete-20 <pedroparty310@gmail.com>
2022-04-23 20:44:41 -0600

    Implementacion de Navbar y Footer para Navbar

--- src/NavigationBar.jsx
+++ src/NavigationBar.jsx
@@ -5,7 +5,8 @@ import "./NavigationBar.sass";
 export default class NavigationBar extends Component {
   render() {
     return (
-      <div className="navigation-bar">
+      <div className="NavigationBar">
+        <div className="navigation-bar">
         <div className="logo-placeholder"></div>
         <div className="navigation-bar-location">
           <div className="location-placeholder-image"></div>
@@ -30,7 +31,46 @@ export default class NavigationBar extends Component {
             <button className="search-button-icon"></button>
           </div>
         </div>
+        <div className="navigation-bar-language">
+          <select className="language-button"></select>
+        </div>
+        <div className="navigation-bar-account">
+          <div className="account-textOne">Hola, Identifícate</div>
+          <div className="account-textTwo">Cuenta y Listas</div>
+        </div>
+        <div className="navigation-bar-returns">
+          <div className="returns-textOne">Devoluciones</div>
+          <div className="returns-textTwo">y Pedidos</div>
+        </div>
+        <div className="navigation-bar-cart">
+          <div className="cart-icon"></div>
+          <div className="cart-number">0</div>
+          <div className="cart-text">Carrito</div>
+        </div>
+        </div>
+        <div className="navigation-bar-footer">
+          <div className="footer-list">
+            <img className="list-icon"></img>
+            <div className="list-text">Todo</div>
+          </div>
+          <div className="footer-list">
+            <button className="text-button-list">Ofertas del Día</button>
+          </div>
+          <div className="footer-list">
+          <button className="text-button-list">Servicio al Cliente</button>
+          </div>
+          <div className="footer-list">
+          <button className="text-button-list">Listas</button>
+          </div>
+          <div className="footer-list">
+          <button className="text-button-list">Tarjetas de Regalo</button>
+          </div>
+          <div className="footer-list">
+          <button className="text-button-list">Vender</button>
+          </div>
+        </div>
       </div>
+      
     );
   }
 }
